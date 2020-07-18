45d60d434f3be23cb281e4ba340372328bdafb58
Ryuind <brullrr@gmail.com>
2020-07-18 09:54:29 -0400

    Updates

---
+++ precache-manifest.1bcc318b28fd926bf3ff56aae636dd7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6944facb5d84385a64a7b306dfecbe41",
+    "url": "/Library-Project-React-pages/index.html"
+  },
+  {
+    "revision": "1592530c5ac61cec85ca",
+    "url": "/Library-Project-React-pages/static/css/main.01c16c19.chunk.css"
+  },
+  {
+    "revision": "9a6d361a253b0f7d66fe",
+    "url": "/Library-Project-React-pages/static/js/2.eaa9ac51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Library-Project-React-pages/static/js/2.eaa9ac51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1592530c5ac61cec85ca",
+    "url": "/Library-Project-React-pages/static/js/main.a8226a62.chunk.js"
+  },
+  {
+    "revision": "b851221c536167ee0cb9",
+    "url": "/Library-Project-React-pages/static/js/runtime-main.2434a615.js"
+  },
+  {
+    "revision": "13ec8e586cd46581cdad63f99af56865",
+    "url": "/Library-Project-React-pages/static/media/book_cover_kawa.13ec8e58.png"
+  }
+]);
